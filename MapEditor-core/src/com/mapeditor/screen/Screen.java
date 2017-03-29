e55ec751b0948938c55d967904e2193bb0f20af1
Mack <mackvalentine@gmail.com>
2017-03-30 00:52:35 +0200

    Renaming

---
+++ MapEditor-core/src/com/mapeditor/screen/Screen.java
@@ -0,0 +1,16 @@
+package com.mapeditor.screen;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public abstract class Screen {
+
+	public abstract void update();
+
+	public abstract void render(SpriteBatch batch);
+
+	public abstract void render(ShapeRenderer shape);
+	
+	public abstract void dispose();
+
+}
